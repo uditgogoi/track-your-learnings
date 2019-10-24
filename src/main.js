2f2b778c741fef6a083d2c5012d0958622493961
udit gogoi <uditgogoi1@gmail.com>
2019-10-25 00:48:21 +0530

    basic structure of the app is done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Home from './view/Home.vue';
+import RecordDetail from './view/DetailPage.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path:'/', component:Home},
+  {path:'/detail', component: RecordDetail}
+];
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
